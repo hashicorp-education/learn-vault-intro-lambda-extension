3d8ec03f7d86224083016940fcd1cd5bbdbb2539
Ken Keller <ken.keller@hashicorp.com>
2025-04-28 09:06:44 -0500

    clean up variables file

--- variables.tf
+++ variables.tf
@@ -8,16 +8,6 @@ variable "environment_name" {
   default = "vault-lambda-extension-demo"
 }
 
-# URL for Vault OSS binary
-variable "vault_zip_file" {
-  default = "https://releases.hashicorp.com/vault/1.6.0/vault_1.6.0_linux_amd64.zip"
-}
-
-# Instance size
-variable "instance_type" {
-  default = "t2.micro"
-}
-
 variable "cloud_provider" {
   description = "The cloud provider of the HCP HVN and Vault cluster."
   type        = string
